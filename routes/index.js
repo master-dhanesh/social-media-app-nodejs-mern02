94d4ec6a4717d296349238e6e1d40bae6bb7ee2f
master-dhanesh <dhanesh1296@gmail.com>
2022-05-21 08:56:02 +0530

    setPassword-changePassword

--- routes/index.js
+++ routes/index.js
@@ -52,18 +52,79 @@ router.post(
     function (req, res, next) {}
 );
 
+// router.post("/login", function (req, res, next) {
+//     passport.authenticate("local", function (err, user, info) {
+//         if (err) return res.send(err);
+//         if (!user) return res.send(info);
+//         req.logIn(user, function (err) {
+//             if (err) return next(err);
+//             res.redirect("/profile");
+//         });
+//     })(req, res, next);
+// });
+
 router.get("/profile", isLoggedIn, function (req, res, next) {
     // console.log(req.user);
     User.find()
         .then((users) => {
-            res.render("profile", { users });
+            res.render("profile", { users, loggedInUser: req.user });
         })
         .catch((err) => res.send(err));
 });
 
-router.get("/logout", function (req, res, next) {
+router.get("/logout", isLoggedIn, function (req, res, next) {
     req.logout();
     res.redirect("/");
+});
+
+router.get("/forgot-password", function (req, res, next) {
+    res.render("forgot");
+});
+
+router.post("/forgot-password", async function (req, res, next) {
+    const user = await User.findOne({ email: req.body.email }).exec();
+    if (!user)
+        return res.send(
+            "User not found! 404 <a href='/forgot-password'>Forgot Password</a>"
+        );
+
+    res.redirect(`/set-password/${user._id}`);
+});
+
+router.get("/set-password/:id", function (req, res, next) {
+    res.render("setpassword", { id: req.params.id });
+});
+
+router.post("/set-password/:id", async function (req, res, next) {
+    try {
+        const user = await User.findById(req.params.id).exec();
+        if (!user)
+            return res.send(
+                "User not found! 404 <a href='/forgot-password'>Forgot Password</a>"
+            );
+        await user.setPassword(req.body.password);
+        await user.save();
+        res.redirect("/login");
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+});
+
+router.get("/change-password", isLoggedIn, function (req, res, next) {
+    res.render("changepassword");
+});
+
+router.post("/change-password/", async function (req, res, next) {
+    try {
+        const user = await User.findById(req.user._id).exec();
+        await user.changePassword(req.body.oldpassword, req.body.newpassword);
+        await user.save();
+        res.redirect("/logout");
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
 });
 
 module.exports = router;
